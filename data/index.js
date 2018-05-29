59bc2165fa77be62367ac904879f0708637b7b19
Alexandr Martynyuk <banifest@gmail.com>
2018-05-29 08:43:19 +0300

    init

--- data/index.js
+++ data/index.js
@@ -6,7 +6,7 @@ const options = {
     dialect: 'mysql',
     logging: false
 };
-const sequelize = new Sequelize('postgres://postgres:admin@localhost:5432/cwp_32');
+const sequelize = new Sequelize('postgres://dutgeskcuenfjf:9b7d1176c9edca7186606fa022f3f5189d55276e4eede7cb62dd8cbc4954a2fd@ec2-50-19-224-165.compute-1.amazonaws.com:5432/d2nr62v7uedcj8');
 
 const Repo = require('./repo')(Sequelize, sequelize);
 const Commit = require('./commit')(Sequelize, sequelize);
